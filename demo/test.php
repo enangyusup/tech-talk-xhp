95f59e31934c9dd544eb6a032f52a430b240224d
Enang Yusup <enang.yusup@gmail.com>
2015-04-09 18:59:25 +0700

    First commit

---
+++ demo/test.php
@@ -0,0 +1,14 @@
+<?hh
+
+require "vendor/autoload.php";
+
+$like_link = 'http://liputan6.com';
+$post =
+  <div class="post">
+    <h2>Hello Word</h2>
+    <p><span>Hey there.</span></p>
+    <a href={$like_link}>Like</a>
+  </div>;
+
+echo $post;
+
